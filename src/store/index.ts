4874a293d8d04bac3120238d348eca8096b5031c
Tr.Nghia <t.nghia2112279@gmail.com>
2026-05-30 11:26:25 +0700

    feat(13-rtk-refactor): connect filtersSlice and uiSlice reducers to configureStore

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,19 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
-import type { Reducer } from 'redux'
 import { useDispatch } from 'react-redux'
 import expensesReducer from './slices/expensesSlice'
-import { filtersReducer } from './filters/reducer'
-import { uiReducer } from './ui/reducer'
-import type { FiltersState } from './filters/reducer'
-import type { UiState } from './ui/reducer'
+import filtersReducer from './slices/filtersSlice'
+import uiReducer from './slices/uiSlice'
 import { STORAGE_KEY } from '../constants'
 
 export const store = configureStore({
   reducer: {
     expenses: expensesReducer,
-    // TODO: Replace filters and ui with createSlice reducers after hands-on #3
-    filters: filtersReducer as Reducer<FiltersState>,
-    ui: uiReducer as Reducer<UiState>,
+    filters: filtersReducer,
+    ui: uiReducer,
   },
 })
 
